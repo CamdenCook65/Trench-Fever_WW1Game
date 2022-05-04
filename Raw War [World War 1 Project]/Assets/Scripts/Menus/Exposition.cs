90b2f90efa5dea3b67e6f16a01c2b51d5745975f
Camden Cook <100425073+CamdenCook65@users.noreply.github.com>
2022-05-04 13:42:28 -0400

    Level Design (Part 4) | | Week 35: Day 3 (5/4) --- Post 2
    I made a good bit of changes to the game to help improve the overall product. I finally decided to create the Main Menu with all of the basic buttons preforming just nicely. I need to get the subMenus for the Levels, Encyclopedia and Credits taken care of sometime. I created a basic fade in and out text system for the Exposition scenes, which will add a nice bit of flavor to the game and allow me to tell a story, albeit a basic one. I also went into the player prefab and made it so that each weapon does not automatically reload when depleted, this was only done for testing purposes. I need to make it known that the player is only meant to have limited ammo during their run per level. I need to sit down and think about how I want to carry out the Encyclopedia, Level Selection and Credits. Do I want them to be subMenus of the MainMenu scene or should they be separate scenes to make things more organized? Anyways, good job!!!

---
+++ Raw War [World War 1 Project]/Assets/Scripts/Menus/Exposition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exposition : MonoBehaviour
+{
+    public int timer = 10;
+    public bool ending = false;
+
+    void Start()
+    {
+        StartCoroutine("Coroutine");
+        
+    }
+
+    IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(timer);
+
+        if(ending == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 27);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Raw War [World War 1 Project]/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MainMenuUI;
+    public GameObject LevelsUI;
+    public GameObject EncyclopediaUI;
+    public GameObject CreditsUI;
+
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadGame()
+    {
+        MainMenuUI.SetActive(false);
+        LevelsUI.SetActive(true);
+        EncyclopediaUI.SetActive(false);
+        CreditsUI.SetActive(false);
+
+    }
+
+    public void Encyclopedia()
+    {
+        MainMenuUI.SetActive(false);
+        LevelsUI.SetActive(false);
+        EncyclopediaUI.SetActive(true);
+        CreditsUI.SetActive(false);
+    }
+
+    public void ReturntoMain()
+    {
+        MainMenuUI.SetActive(true);
+        LevelsUI.SetActive(false);
+        EncyclopediaUI.SetActive(false);
+        CreditsUI.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        MainMenuUI.SetActive(false);
+        LevelsUI.SetActive(false);
+        EncyclopediaUI.SetActive(false);
+        CreditsUI.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game!");
+    }
+}
