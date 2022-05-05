798d88b25ae94f9a9f109e074ce6e68efd0e5247
Camden Cook <100425073+CamdenCook65@users.noreply.github.com>
2022-05-05 17:41:04 -0400

    Level Design (Part 4) | | Week 35: Day 4 (5/5) --- Post 1
    I updated the Level loading system so that the player can choose which level they want to play in. I'm considering whether I should spend time developing a system that unlocks levels but this will be unlikely.

---
+++ Raw War [World War 1 Project]/Assets/Scripts/Menus/LoadLevels.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevels : MonoBehaviour
+{
+    public void ReturntoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void C1L1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void C1L2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void C1L3()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void C1L4()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void C1L5()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void C2L1()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void C2L2()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void C2L3()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void C2L4()
+    {
+        SceneManager.LoadScene(11);
+    }
+
+    public void C2L5()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    public void C3L1()
+    {
+        SceneManager.LoadScene(14);
+    }
+
+    public void C3L2()
+    {
+        SceneManager.LoadScene(15);
+    }
+
+    public void C3L3()
+    {
+        SceneManager.LoadScene(16);
+    }
+
+    public void C3L4()
+    {
+        SceneManager.LoadScene(17);
+    }
+
+    public void C3L5()
+    {
+        SceneManager.LoadScene(18);
+    }
+
+    public void C4L1()
+    {
+        SceneManager.LoadScene(20);
+    }
+
+    public void C4L2()
+    {
+        SceneManager.LoadScene(21);
+    }
+
+    public void C4L3()
+    {
+        SceneManager.LoadScene(22);
+    }
+
+    public void C4L4()
+    {
+        SceneManager.LoadScene(23);
+    }
+
+    public void C4L5()
+    {
+        SceneManager.LoadScene(24);
+    }
+
+    public void C5L0()
+    {
+        SceneManager.LoadScene(26);
+    }
+}
