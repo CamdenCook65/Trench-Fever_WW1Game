e974efcbf8861bb644f288f5634ec28dfd75d10c
Camden Cook <100425073+CamdenCook65@users.noreply.github.com>
2022-06-08 18:10:56 -0400

    Finishing Touches (Part 1) | | Week 40: Day 3 (6/8) --- Post 2
    I added a milliseconds system to the WinEvent to encourage speedrunning

--- Raw War [World War 1 Project]/Assets/Scripts/Timer.cs
+++ Raw War [World War 1 Project]/Assets/Scripts/Timer.cs
@@ -21,10 +21,16 @@ public class Timer : MonoBehaviour
         if (playing == true)
         {
             //The clock ticks upwards, Minutes:Seconds in place of Hours:Minutes
+            //I later added Milliseconds to the fray to better encourage speedrunners
             TimerClock += Time.deltaTime * speed;
+
             string minutes = Mathf.Floor((TimerClock % 3600) / 60).ToString("0");
-            string seconds = (TimerClock % 60).ToString("00");
-            text.text = "Time" + " " + minutes + ":" + seconds;
+            string seconds = Mathf.Floor((TimerClock % 60)).ToString("00");
+
+            //string seconds = (TimerClock % 60).ToString("00");
+            string milliseconds = (Mathf.Floor(TimerClock * 100) % 100).ToString("00");
+
+            text.text = "Time" + " " + minutes + ":" + seconds + ":" + milliseconds;
         }
     }
 }
