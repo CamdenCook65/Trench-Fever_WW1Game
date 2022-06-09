3d7637ecca78320a2c5769805872911266ea3f95
Camden Cook <100425073+CamdenCook65@users.noreply.github.com>
2022-06-09 14:28:02 -0400

    Finishing Touches (Part 1) | | Week 40: Day 4 (6/9) --- Post 1
    Created a pause system for my Game, now need to copy and paste it throughout all 21 levels.

---
+++ Raw War [World War 1 Project]/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool pausing = false;
+    public Text text;
+    public float TimerClock = 3;
+    public float speed = -1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            pausing = true;
+            pauseMenu.SetActive(true);
+            StartCoroutine("ExampleCoroutine");
+        }
+        else if (!Input.GetKey(KeyCode.Escape))
+        {
+            pausing = false;
+            StopCoroutine("ExampleCoroutine");
+            pauseMenu.SetActive(false);
+        }
+
+
+        if (pausing == false)
+        {
+            TimerClock = 4;
+        }
+
+        if (pausing == true)
+        {
+            TimerClock += Time.deltaTime * speed;
+            string seconds = Mathf.Floor((TimerClock % 60)).ToString("0");
+            text.text = seconds;
+        }
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+
+            SceneManager.LoadScene(0);
+
+        }
+
+
+    }
+}
