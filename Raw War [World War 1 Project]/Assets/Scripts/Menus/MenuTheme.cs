6a9b87eca4f7ce456ef9c62c5a47ec40999c9865
Camden Cook <100425073+CamdenCook65@users.noreply.github.com>
2022-06-10 22:11:01 -0400

    Finishing Touches (Part 1) | | Week 40: Day 5 (6/10) --- Post 2
    I began making some nice touches to the Encyclopedia, Credits and LoadLevel Menus. I added a really neat system where the music will seamlessly travel between the four menu scenes without much issue. It took a while but the end result is pretty amazing. Can't wait to see what the finished product is going to look like.

---
+++ Raw War [World War 1 Project]/Assets/Scripts/Menus/MenuTheme.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTheme : MonoBehaviour
+{
+    public GameObject instance;
+    public int CheckValue;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        //CheckValue = SceneManager.GetActiveScene().buildIndex;
+
+        if (instance == null)
+        {
+            instance = GameObject.Find("MainMenuTheme");
+        }
+
+        if (instance != null && instance == this.gameObject)
+        {
+            //Do Nothing
+        }
+        else
+        {
+            //Debug.Log("It's my Fault!");
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    //Checks if the current scene is one of the menu scenes. If not, then the gameObject will be destroyed.
+    void Update()
+    {
+        CheckValue = SceneManager.GetActiveScene().buildIndex;
+
+        if ((CheckValue == 0) || (CheckValue == 30) || (CheckValue == 31) || (CheckValue == 32))
+        {
+            //Do Nothing
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Raw War [World War 1 Project]/Assets/Scripts/Menus/ThemeOrganizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeOrganizer : MonoBehaviour
+{
+    public AudioSource instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<AudioSource>();
+        }
+
+        if(instance != null && instance != this)
+        {
+               //Destroy(gameObject);
+        }
+    }
+}
