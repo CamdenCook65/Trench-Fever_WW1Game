c87a3fdd178b20dd97c4bece1bf0927ef53ec182
Camden Cook <100425073+CamdenCook65@users.noreply.github.com>
2022-06-28 15:44:11 -0400

    Finishing Touches (Part 4) | | Week 43: Day 2 (6/28) --- Post 4
    I created a script so that the music will slowly fade in the exposition scenes, so that their is a nice flow between scenes. Now I only have a few minor things to work on before I can call this game finished.
    
    1. Smooth down Terrain in Levels
    2. Replace Win Screen Music
    3. Flesh out Exposition Text
    4. Add in Background Objects

---
+++ Raw War [World War 1 Project]/Assets/Scripts/Menus/MusicFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicFade : MonoBehaviour
+{
+    public AudioSource audiosource;
+    public int waitTime = 6;
+
+
+    void Start()
+    {
+        StartCoroutine("Coroutine"); 
+    }
+
+    IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        while (audiosource.volume > 0)
+        {
+            yield return null;
+            audiosource.volume-= Time.deltaTime;
+        }
+    }
+
+
+}
